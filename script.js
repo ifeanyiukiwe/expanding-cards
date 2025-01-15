7d462323f8156246381e03f29e5117fe0b839766
ifeanyiukiwe <duio1541@gmail.com>
2025-01-15 05:30:30 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+alert("Expanding Card");
+
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+    panel.addEventListener('mouseover', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
